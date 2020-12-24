b13e0d3bef4c075e32d3c39196a48646e6469929
ydnaod <65431123+ydnaod@users.noreply.github.com>
2020-12-24 17:27:10 -0500

    add landing page functionality

--- mobileAmblerLP1.js
+++ mobileAmblerLP1.js
@@ -8,16 +8,50 @@ const testForms = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   //await page.goto('http://m.thebeautyinstituteskp.edu/campus/ambler-beauty-schools?lead_src=google-mobile');
-  await page.goto('http://m.thebeautyinstituteskp.edu/campus/ambler-beauty-schools/cosmetology?lead_src=facebook-mobile');
-  await page.type('#ambler_firstname', 'AndyTestFacebook'); 
-  await page.type('#ambler_lastname', 'AmblerCOSMOBStartCareerNow'); 
+  //await page.goto('http://m.thebeautyinstituteskp.edu/campus/ambler-beauty-schools/cosmetology?lead_src=facebook-mobile');
+  await page.goto('http://m.thebeautyinstituteskp.edu/campus/ambler-beauty-schools/esthetics?lead_src=google-mobile');
+  await page.type('#ambler_firstname', 'AndyTestGoogle'); 
+  await page.type('#ambler_lastname', 'AmblerSkinMOBStartCareerNow'); 
   await page.type('#ambler_phone', '9086709534'); 
   await page.type('#ambler_email', 'ydnaod12@gmail.com'); 
-  await page.type('#ambler_interest_id', 'Cosmetology'); 
 
   await page.click('#bt_submit');
   console.log('lets gooo');
   await page.screenshot({path: 'ambler.png'});
+  await page.waitForTimeout(10000);
+
+  await page.goto('http://m.thebeautyinstituteskp.edu/campus/ambler-beauty-schools/cosmetology?lead_src=google-mobile');
+  await page.type('#ambler_firstname', 'AndyTestGoogle'); 
+  await page.type('#ambler_lastname', 'AmblerCosMOBStartCareerNow'); 
+  await page.type('#ambler_phone', '9086709534'); 
+  await page.type('#ambler_email', 'ydnaod12@gmail.com'); 
+
+  await page.click('#bt_submit');
+  console.log('lets gooo');
+  await page.screenshot({path: 'ambler.png'});
+  await page.waitForTimeout(10000);
+
+  await page.goto('http://m.thebeautyinstituteskp.edu/campus/philadelphia-beauty-schools/esthetics?lead_src=google-mobile');
+  await page.type('#ambler_firstname', 'AndyTestGoogle'); 
+  await page.type('#ambler_lastname', 'PhillySkinMOBStartCareerNow'); 
+  await page.type('#ambler_phone', '9086709534'); 
+  await page.type('#ambler_email', 'ydnaod12@gmail.com'); 
+
+  await page.click('#bt_submit');
+  console.log('lets gooo');
+  await page.screenshot({path: 'ambler.png'});
+  await page.waitForTimeout(10000);
+
+  await page.goto('http://m.thebeautyinstituteskp.edu/campus/philadelphia-beauty-schools/cosmetology?lead_src=google-mobile');
+  await page.type('#ambler_firstname', 'AndyTestGoogle'); 
+  await page.type('#ambler_lastname', 'PhillyCosMOBStartCareerNow'); 
+  await page.type('#ambler_phone', '9086709534'); 
+  await page.type('#ambler_email', 'ydnaod12@gmail.com'); 
+
+  await page.click('#bt_submit');
+  console.log('lets gooo');
+  await page.screenshot({path: 'ambler.png'});
+  await page.waitForTimeout(10000);
   // other actions...
   await browser.close();
 };
--- mobileAmblerLP2.js
+++ mobileAmblerLP2.js
@@ -7,16 +7,50 @@ const puppeteer = require('puppeteer');
 const testForms = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  //await page.goto('http://m.thebeautyinstituteskp.edu/campus/ambler-beauty-schools?lead_src=google-mobile');
+  //await page.goto('http://m.thebeautyinstituteskp.edu/campus/ambler-beauty-schools?lead_src=facebook-mobile');
   await page.goto('http://m.thebeautyinstituteskp.edu/campus/ambler-beauty-schools/esthetics?lead_src=facebook-mobile');
-  await page.type('#ambler_firstname', 'AndyTestFacebook'); 
-  await page.type('#ambler_lastname', 'AmblerCOSMOBStartCareerNow'); 
+  await page.type('#ambler_firstname', 'AndyTestfacebook'); 
+  await page.type('#ambler_lastname', 'AmblerSkinMOBStartCareerNow'); 
+  await page.type('#ambler_phone', '9086709534'); 
+  await page.type('#ambler_email', 'ydnaod12@gmail.com'); 
+
+  await page.click('#bt_submit');
+  console.log('lets gooo!');
+  await page.screenshot({path: 'ambler.png'});
+  await page.waitForTimeout(10000);
+
+  await page.goto('http://m.thebeautyinstituteskp.edu/campus/ambler-beauty-schools/cosmetology?lead_src=facebook-mobile');
+  await page.type('#ambler_firstname', 'AndyTestfacebook'); 
+  await page.type('#ambler_lastname', 'AmblerCosMOBStartCareerNow'); 
   await page.type('#ambler_phone', '9086709534'); 
   await page.type('#ambler_email', 'ydnaod12@gmail.com'); 
 
   await page.click('#bt_submit');
   console.log('lets gooo');
   await page.screenshot({path: 'ambler.png'});
+  await page.waitForTimeout(10000);
+
+  await page.goto('http://m.thebeautyinstituteskp.edu/campus/philadelphia-beauty-schools/esthetics?lead_src=facebook-mobile');
+  await page.type('#ambler_firstname', 'AndyTestfacebook'); 
+  await page.type('#ambler_lastname', 'PhillySkinMOBStartCareerNow'); 
+  await page.type('#ambler_phone', '9086709534'); 
+  await page.type('#ambler_email', 'ydnaod12@gmail.com'); 
+
+  await page.click('#bt_submit');
+  console.log('lets gooo');
+  await page.screenshot({path: 'ambler.png'});
+  await page.waitForTimeout(10000);
+
+  await page.goto('http://m.thebeautyinstituteskp.edu/campus/philadelphia-beauty-schools/cosmetology?lead_src=facebook-mobile');
+  await page.type('#ambler_firstname', 'AndyTestfacebook'); 
+  await page.type('#ambler_lastname', 'PhillyCosMOBStartCareerNow'); 
+  await page.type('#ambler_phone', '9086709534'); 
+  await page.type('#ambler_email', 'ydnaod12@gmail.com'); 
+
+  await page.click('#bt_submit');
+  console.log('lets gooo');
+  await page.screenshot({path: 'ambler.png'});
+  await page.waitForTimeout(10000);
   // other actions...
   await browser.close();
 };
